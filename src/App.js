3921d85b7e367a914dacdbbb724fccbdc02dbacf
liuweishi <1434967721@qq.com>
2020-04-27 20:58:42 +0800

    更新

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import {Button} from 'antd'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        App...
+        刘维师
+        <Button type='primary'>点我</Button>
       </div>
+      
     )
   }
 }
